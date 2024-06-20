6bdee51d0eb5bca921eb910df18af5b686110846
ShamNeko <mkdvlp01@gmail.com>
2024-06-20 22:47:56 +0900

    ヘッドレス

--- web_access.py
+++ web_access.py
@@ -1,6 +1,7 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 class WebAccess():
@@ -8,12 +9,27 @@ class WebAccess():
         pass
 
     def upd_web_driver(self) -> str:
-        # webドライバーの更新
+        # WebDriverのパスを取得
         driver_path = ChromeDriverManager().install()
         return driver_path
 
     def access(self, driver_path: str, url: str, waiting_time: int = 3):
-        driver = webdriver.Chrome(service=Service(executable_path=driver_path))
+        # オプションを設定
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--log-level=3')
+
+        # WebDriverを初期化
+        driver = webdriver.Chrome(
+            service=Service(executable_path=driver_path),
+            options=options
+        )
         driver.get(url)
-        # ドライバーが要素を探す時間を確保（htmlタグ等）
         driver.implicitly_wait(waiting_time)
+        # ドライバーが要素を探す時間を確保（htmlタグ等）
+
+
+if __name__ == "__main__":
+    wa = WebAccess()
+    driver_path = wa.upd_web_driver()
+    wa.access(driver_path, "https://google.com")
